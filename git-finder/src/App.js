f5ca30d9ef430e51639941ebce6921a0e0918a90
Srijan Tripathi <47470062+srijan2498@users.noreply.github.com>
2024-10-25 04:25:43 +0530

    Add files via upload

--- git-finder/src/App.js
+++ git-finder/src/App.js
@@ -1,14 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Hero from './components/Hero';
 import User from './components/User';
 import { uData, rData } from './components/data';
 import Repo from './components/Repo';
 import Footer from './components/Footer';
+import axios from 'axios';
 
 function App() {
   const [userData, setUserData] = useState(uData)
   const [userRepoData, setUserRepoData] = useState(rData)
+
+  const fetchData=async()=>{
+    try {
+      const fetchedData = await axios(`https://api.github.com/users/srijan2498`)
+      setUserData(fetchedData.data)
+      const fetchedRepo = await axios(`https://api.github.com/users/srijan2498/repos`)
+      setUserRepoData(fetchedRepo.data)
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <Hero setUserData={setUserData} setUserRepoData={setUserRepoData} />
--- git-finder/src/components/Footer.js
+++ git-finder/src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
     <div className='footer'>
-          © 2023 <span className="company">Find My Git</span> Developed by Srijan :)
+      © {year} <span className="company">Find My Git</span> Developed by Srijan :)
     </div>
   )
 }
